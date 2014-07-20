4f596aa9fdc88c7600f1c891681c783900a0863b
Patrick Drechsler <patrick@pdrechsler.de>
2014-07-20 20:40:08 +0200

    api: locationsReadOne

--- app_api/controllers/locations.js
+++ app_api/controllers/locations.js
@@ -3,20 +3,40 @@
 var mongoose = require('mongoose');
 var Loc = mongoose.model('location');
 
-var sendJSONresponse = function (res, status, content) {
+var sendJSONresponse = function(res, status, content) {
   res.status(status);
   res.json(content);
 };
 
-module.exports.locationsCreate = function (req, res) {};
-module.exports.locationsListByDistance = function (req, res) {};
-module.exports.locationsReadOne = function (req, res) {
-  sendJSONresponse(res, 200, {"status": "success"});
-};
-module.exports.locationsUpdateOne = function (req, res) {};
-module.exports.locationsDeleteOne = function (req, res) {};
+module.exports.locationsCreate = function(req, res) {};
+module.exports.locationsListByDistance = function(req, res) {};
 
-module.exports.reviewsCreate = function (req, res) {};
-module.exports.reviewsReadOne = function (req, res) {};
-module.exports.reviewsUpdateOne = function (req, res) {};
-module.exports.reviewsDeleteOne = function (req, res) {};
+module.exports.locationsReadOne = function(req, res) {
+  if (req.params && req.params.locationid) {
+    Loc.findById(req.params.locationid)
+      .exec(function(err, location) {
+        if (!location) {
+          sendJSONresponse(res, 404, {
+            "message": "location not found"
+          });
+          return;
+        } else if (err) {
+          sendJSONresponse(res, 404, err);
+          return;
+        }
+        sendJSONresponse(res, 200, location);
+      });
+    } else {
+      sendJSONresponse(res, 404, {
+        "message": "No locationid in request"
+      });
+    }
+};
+
+module.exports.locationsUpdateOne = function(req, res) {};
+module.exports.locationsDeleteOne = function(req, res) {};
+
+module.exports.reviewsCreate = function(req, res) {};
+module.exports.reviewsReadOne = function(req, res) {};
+module.exports.reviewsUpdateOne = function(req, res) {};
+module.exports.reviewsDeleteOne = function(req, res) {};
