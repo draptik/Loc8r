b164f06e66f0b9e3172fe973960964edf9e21a58
Patrick Drechsler <patrick@pdrechsler.de>
2014-07-20 18:33:06 +0200

    api: controller placeholder

---
+++ app_api/controllers/locations.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports.locationsCreate = function (req, res) {};
+module.exports.locationsListByDistance = function (req, res) {};
+module.exports.locationsReadOne = function (req, res) {};
+module.exports.locationsUpdateOne = function (req, res) {};
+module.exports.locationsDeleteOne = function (req, res) {};
+
+module.exports.reviewsCreate = function (req, res) {};
+module.exports.reviewsReadOne = function (req, res) {};
+module.exports.reviewsUpdateOne = function (req, res) {};
+module.exports.reviewsDeleteOne = function (req, res) {};
